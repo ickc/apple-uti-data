3e462834f54b788e20df84a01d492c300fdf593d
Kolen Cheung <christian.kolen@gmail.com>
2021-07-16 01:45:46 -0700

    add merge_data

--- src/apple_uti/util.py
+++ src/apple_uti/util.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from collections import defaultdict
 from itertools import chain
 
 if TYPE_CHECKING:
-    from typing import Union, Iterable, Any
+    from typing import Union, Iterable, Any, Dict, Set
 
 
 def union(*args: Iterable) -> set:
     """A union function similar to set.union method."""
     return set(chain.from_iterable(*args))
+
+
+def merge_data(*args: Dict[str, Set[str]]) -> Dict[str, Set[str]]:
+    res: Dict[str, Set[str]] = defaultdict(set)
+    for data in args:
+        for key, value in data.items():
+            res[key] |= value
+    return res
 
 
 def stringify(data: Union[list, dict, str, Any]) -> Union[list, dict, str]:
